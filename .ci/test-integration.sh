296ea474ad577ca319d9b119480a50df2a0c8f8c
Tim Ebert <tim.ebert@sap.com>
2020-11-26 16:05:12 +0100

    Run infra integration test on release

---
+++ .ci/test-integration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cd "$(dirname $0)/.."
+
+mkdir -p /tm
+/cc/utils/cli.py config attribute --cfg-type kubernetes --cfg-name testmachinery --key kubeconfig > /tm/kubeconfig
+/testrunner run \
+    --tm-kubeconfig-path=/tm/kubeconfig \
+    --no-execution-group \
+    --testrun-prefix tm-extension-aws- \
+    --timeout=1800 \
+    --testruns-chart-path=.ci/testruns/default \
+    --set revision="$(cat ./VERSION)"
