645556bf922a33d6df87cf6d4ce5bcc254faf411
Stoyan Rachev <s.rachev@sap.com>
2021-06-10 13:49:27 +0300

    Add validation of Kubernetes feature gates

---
+++ pkg/apis/aws/validation/controlplane_test.go
@@ -0,0 +1,65 @@
+// Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validation_test
+
+import (
+	apisaws "github.com/gardener/gardener-extension-provider-aws/pkg/apis/aws"
+	. "github.com/gardener/gardener-extension-provider-aws/pkg/apis/aws/validation"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+var _ = Describe("ControlPlaneConfig validation", func() {
+	var (
+		controlPlane *apisaws.ControlPlaneConfig
+		fldPath      *field.Path
+	)
+
+	BeforeEach(func() {
+		controlPlane = &apisaws.ControlPlaneConfig{}
+	})
+
+	Describe("#ValidateControlPlaneConfig", func() {
+		It("should return no errors for a valid configuration", func() {
+			Expect(ValidateControlPlaneConfig(controlPlane, "", fldPath)).To(BeEmpty())
+		})
+
+		It("should fail with invalid CCM feature gates", func() {
+			controlPlane.CloudControllerManager = &apisaws.CloudControllerManagerConfig{
+				FeatureGates: map[string]bool{
+					"AnyVolumeDataSource":      true,
+					"CustomResourceValidation": true,
+					"Foo":                      true,
+				},
+			}
+
+			errorList := ValidateControlPlaneConfig(controlPlane, "1.18.14", fldPath)
+
+			Expect(errorList).To(ConsistOf(
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":  Equal(field.ErrorTypeForbidden),
+					"Field": Equal("cloudControllerManager.featureGates.CustomResourceValidation"),
+				})),
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":  Equal(field.ErrorTypeInvalid),
+					"Field": Equal("cloudControllerManager.featureGates.Foo"),
+				})),
+			))
+		})
+	})
+})
