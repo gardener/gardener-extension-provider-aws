5c0592efed7c5d0a23f1d66a876aa4f07d158790
Kistner, Dominic <dominic.kistner@sap.com>
2022-04-06 08:38:54 +0200

    Upgrade `v1beta1` vpa resources to `v1`

--- cmd/gardener-extension-provider-aws/app/app.go
+++ cmd/gardener-extension-provider-aws/app/app.go
@@ -46,7 +46,7 @@ import (
 	"github.com/spf13/cobra"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	autoscalingv1beta2 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1beta2"
+	autoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"k8s.io/component-base/version/verflag"
@@ -187,7 +187,7 @@ func NewControllerManagerCommand(ctx context.Context) *cobra.Command {
 			if err := druidv1alpha1.AddToScheme(scheme); err != nil {
 				return fmt.Errorf("could not update manager scheme: %w", err)
 			}
-			if err := autoscalingv1beta2.AddToScheme(scheme); err != nil {
+			if err := autoscalingv1.AddToScheme(scheme); err != nil {
 				return fmt.Errorf("could not update manager scheme: %w", err)
 			}
 			if err := machinev1alpha1.AddToScheme(scheme); err != nil {
