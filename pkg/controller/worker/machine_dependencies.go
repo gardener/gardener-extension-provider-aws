70dc5168ab041e122fe4e96ce4b47bee1b0ca6c1
Shafeeque E S <shafeeque.e.s@sap.com>
2022-08-01 18:38:04 +0530

    Implement modified WorkerDelegate

--- pkg/controller/worker/machine_dependencies.go
+++ pkg/controller/worker/machine_dependencies.go
@@ -25,3 +25,23 @@ func (w *workerDelegate) DeployMachineDependencies(_ context.Context) error {
 func (w *workerDelegate) CleanupMachineDependencies(_ context.Context) error {
 	return nil
 }
+
+// PreReconcileHook implements genericactuator.WorkerDelegate.
+func (w *workerDelegate) PreReconcileHook(_ context.Context) error {
+	return nil
+}
+
+// PostReconcileHook implements genericactuator.WorkerDelegate.
+func (w *workerDelegate) PostReconcileHook(_ context.Context) error {
+	return nil
+}
+
+// PreDeleteHook implements genericactuator.WorkerDelegate.
+func (w *workerDelegate) PreDeleteHook(_ context.Context) error {
+	return nil
+}
+
+// PostDeleteHook implements genericactuator.WorkerDelegate.
+func (w *workerDelegate) PostDeleteHook(_ context.Context) error {
+	return nil
+}
