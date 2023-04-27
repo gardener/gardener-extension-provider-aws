c115f124b471667921a5731c2eeb8520da150ba0
C5336947 <akin.alalade@sap.com>
2023-04-27 16:33:40 -0400

    Add permission to write logs to CloudWatch by default

--- pkg/controller/infrastructure/templates/main.tpl.tf
+++ pkg/controller/infrastructure/templates/main.tpl.tf
@@ -334,7 +334,12 @@ resource "aws_iam_role_policy" "nodes" {
     {
       "Effect": "Allow",
       "Action": [
-        "ec2:DescribeInstances"
+        "ec2:DescribeInstances",
+        "logs:CreateLogStream",
+        "logs:DescribeLogStreams",
+        "logs:PutLogEvents",
+        "logs:PutRetentionPolicy",
+        "logs:CreateLogGroup"
       ],
       "Resource": [
         "*"
