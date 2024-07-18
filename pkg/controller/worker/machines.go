5124bed0f9f57721d9782f30d96fa534001b1b86
elankath <tarun.ramakrishna.elankath@sap.com>
2024-07-18 11:05:52 +0530

    support extended resources without re-specifying core resources

--- pkg/controller/worker/machines.go
+++ pkg/controller/worker/machines.go
@@ -7,6 +7,7 @@ package worker
 import (
 	"context"
 	"fmt"
+	"maps"
 	"path/filepath"
 	"sort"
 	"strconv"
@@ -186,21 +187,20 @@ func (w *workerDelegate) generateMachineConfig(ctx context.Context) error {
 				machineClassSpec["keyName"] = infrastructureStatus.EC2.KeyName
 			}
 
-			if workerConfig.NodeTemplate != nil {
-				machineClassSpec["nodeTemplate"] = machinev1alpha1.NodeTemplate{
-					Capacity:     workerConfig.NodeTemplate.Capacity,
-					InstanceType: pool.MachineType,
-					Region:       w.worker.Spec.Region,
-					Zone:         zone,
-				}
-			} else if pool.NodeTemplate != nil {
-				machineClassSpec["nodeTemplate"] = machinev1alpha1.NodeTemplate{
+			var nodeTemplate machinev1alpha1.NodeTemplate
+			if pool.NodeTemplate != nil {
+				nodeTemplate = machinev1alpha1.NodeTemplate{
 					Capacity:     pool.NodeTemplate.Capacity,
 					InstanceType: pool.MachineType,
 					Region:       w.worker.Spec.Region,
 					Zone:         zone,
 				}
 			}
+			if workerConfig.NodeTemplate != nil {
+				// Support providerConfig extended resources by copying into node template capacity
+				maps.Copy(nodeTemplate.Capacity, workerConfig.NodeTemplate.Capacity)
+			}
+			machineClassSpec["nodeTemplate"] = nodeTemplate
 
 			if cpuOptions := workerConfig.CpuOptions; cpuOptions != nil {
 				machineClassSpec["cpuOptions"] = map[string]int64{
