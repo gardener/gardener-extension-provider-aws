e943ddbf55875eb6634d7e60bbbdb32e1edd6d29
Alexander Hebel <a.hebelsun@gmail.com>
2024-11-07 16:58:58 +0100

    Address sast: credential findings

--- pkg/webhook/controlplane/ensurer.go
+++ pkg/webhook/controlplane/ensurer.go
@@ -42,8 +42,8 @@ import (
 )
 
 const (
-	ecrCredentialConfigLocation = "/opt/gardener/ecr-credential-provider-config.json"
-	ecrCredentialBinLocation    = "/opt/bin/"
+	ecrCredentialConfigLocation = "/opt/gardener/ecr-credential-provider-config.json" // #nosec G101 -- No credential.
+	ecrCredentialBinLocation    = "/opt/bin/"                                         // #nosec G101 -- No credential.
 )
 
 var (
