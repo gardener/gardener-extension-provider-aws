98a5560b950116ecdb12e85708b29cc72ea64147
Matthias Horne <matthias.horne@sap.com>
2025-02-12 12:45:59 +0100

    Add simple test for object selector

---
+++ pkg/webhook/shootservice/add_test.go
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package shootservice
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/rest"
+	manager "sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+var _ = Describe("Add shoot service webhook", func() {
+	It("should return webhook with ObjectSelector", func() {
+		mgr, err := manager.New(&rest.Config{}, manager.Options{})
+		Expect(err).ToNot(HaveOccurred())
+
+		webhook, err := AddToManager(mgr)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(webhook).ToNot(BeNil())
+		Expect(webhook.ObjectSelector).ToNot(BeNil())
+	})
+})
