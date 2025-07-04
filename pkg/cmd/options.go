ed7c779ea038ba368b72a0f251b5c07079343c3c
Shafeeque E S <shafeeque.e.s@sap.com>
2025-07-04 13:26:09 +0530

    Validate extension BackupBucket

--- pkg/cmd/options.go
+++ pkg/cmd/options.go
@@ -32,6 +32,7 @@ import (
 	healthcheckcontroller "github.com/gardener/gardener-extension-provider-aws/pkg/controller/healthcheck"
 	infrastructurecontroller "github.com/gardener/gardener-extension-provider-aws/pkg/controller/infrastructure"
 	workercontroller "github.com/gardener/gardener-extension-provider-aws/pkg/controller/worker"
+	"github.com/gardener/gardener-extension-provider-aws/pkg/webhook/backupbucket"
 	cloudproviderwebhook "github.com/gardener/gardener-extension-provider-aws/pkg/webhook/cloudprovider"
 	controlplanewebhook "github.com/gardener/gardener-extension-provider-aws/pkg/webhook/controlplane"
 	"github.com/gardener/gardener-extension-provider-aws/pkg/webhook/infrastructure"
@@ -75,6 +76,7 @@ func WebhookSwitchOptions() *webhookcmd.SwitchOptions {
 		webhookcmd.Switch(configmapwebhook.WebhookName, configmapwebhook.AddToManager),
 		webhookcmd.Switch(extensionscloudproviderwebhook.WebhookName, cloudproviderwebhook.AddToManager),
 		webhookcmd.Switch(infrastructure.WebhookName, infrastructure.AddToManager),
+		webhookcmd.Switch(backupbucket.WebhookName, backupbucket.AddToManager),
 		webhookcmd.Switch(terraformerwebhook.WebhookName, terraformerwebhook.AddToManager),
 	)
 }
