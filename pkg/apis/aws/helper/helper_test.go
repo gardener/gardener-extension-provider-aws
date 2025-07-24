dd7dc94f049d7c8cf59f8211446f9015441d788c
Shafeeque E S <shafeeque.e.s@sap.com>
2025-07-24 17:21:41 +0530

    Validate `core.BackupBucket` and `providerConfig` field (#1388)
    * Nit: Remove leftover
    
    * Extract `DecodeBackupBucketConfig` to helper
    
    * Extract validations out of seed validator
    
    * Validate core BackupBucket
    
    * Validate extension BackupBucket
    
    * Revert "Validate extension BackupBucket"
    
    This reverts commit a1e500c776b631155c671b1f626a11de09987f68.

--- pkg/apis/aws/helper/helper_test.go
+++ pkg/apis/aws/helper/helper_test.go
@@ -5,12 +5,20 @@
 package helper_test
 
 import (
+	"reflect"
+	"time"
+
+	"github.com/gardener/gardener/pkg/apis/core"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/utils/ptr"
 
 	api "github.com/gardener/gardener-extension-provider-aws/pkg/apis/aws"
 	. "github.com/gardener/gardener-extension-provider-aws/pkg/apis/aws/helper"
+	apiv1alpha1 "github.com/gardener/gardener-extension-provider-aws/pkg/apis/aws/v1alpha1"
 )
 
 var _ = Describe("Helper", func() {
@@ -113,7 +121,69 @@ var _ = Describe("Helper", func() {
 		Entry("volume found (single entry)", []api.DataVolume{{Name: "foo"}}, "foo", &api.DataVolume{Name: "foo"}),
 		Entry("volume found (multiple entries)", []api.DataVolume{{Name: "bar"}, {Name: "foo"}, {Name: "baz"}}, "foo", &api.DataVolume{Name: "foo"}),
 	)
+
+	Describe("Decode", func() {
+		var (
+			decoder runtime.Decoder
+			scheme  *runtime.Scheme
+		)
+
+		BeforeEach(func() {
+			scheme = runtime.NewScheme()
+			Expect(core.AddToScheme(scheme)).To(Succeed())
+			Expect(api.AddToScheme(scheme)).To(Succeed())
+			Expect(apiv1alpha1.AddToScheme(scheme)).To(Succeed())
+
+			decoder = serializer.NewCodecFactory(scheme, serializer.EnableStrict).UniversalDecoder()
+
+		})
+
+		DescribeTable("DecodeBackupBucketConfig",
+			func(config *runtime.RawExtension, want *api.BackupBucketConfig, wantErr bool) {
+				got, err := DecodeBackupBucketConfig(decoder, config)
+				if wantErr {
+					Expect(err).To(HaveOccurred())
+				} else {
+					Expect(err).NotTo(HaveOccurred())
+				}
+				Expect(equalBackupBucketConfig(got, want)).To(BeTrue())
+			},
+			Entry("valid config", &runtime.RawExtension{Raw: []byte(`{"apiVersion": "aws.provider.extensions.gardener.cloud/v1alpha1","kind": "BackupBucketConfig", "immutability": {"retentionType": "bucket", "retentionPeriod": "24h", "mode": "compliance"}}`)},
+				&api.BackupBucketConfig{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "aws.provider.extensions.gardener.cloud/v1alpha1",
+						Kind:       "BackupBucketConfig",
+					},
+					Immutability: &api.ImmutableConfig{
+						RetentionType:   "bucket",
+						RetentionPeriod: metav1.Duration{Duration: 24 * time.Hour},
+						Mode:            "compliance",
+					},
+				}, false),
+			Entry("invalid config", &runtime.RawExtension{Raw: []byte(`invalid`)}, nil, true),
+			Entry("missing fields", &runtime.RawExtension{Raw: []byte(`{"apiVersion": "aws.provider.extensions.gardener.cloud/v1alpha1","kind": "BackupBucketConfig"}`)},
+				&api.BackupBucketConfig{
+					TypeMeta: metav1.TypeMeta{
+						APIVersion: "aws.provider.extensions.gardener.cloud/v1alpha1",
+						Kind:       "BackupBucketConfig",
+					},
+				}, false),
+			Entry("different data in provider config", &runtime.RawExtension{Raw: []byte(`{"apiVersion": "aws.provider.extensions.gardener.cloud/v1alpha1", "kind": "DifferentConfig", "someField": "someValue"}`)},
+				nil, true),
+		)
+	})
 })
+
+func equalBackupBucketConfig(a, b *api.BackupBucketConfig) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+
+	return reflect.DeepEqual(a.Immutability, b.Immutability)
+}
 
 //nolint:unparam
 func makeProfileMachineImages(name, version, region, ami string, arch *string) []api.MachineImages {
