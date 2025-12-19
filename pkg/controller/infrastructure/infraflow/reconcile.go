1a2e86241568c474c78842373f648372a64887a7
Alexander Hebel <a.hebelsun@gmail.com>
2025-12-19 09:11:02 +0100

    Enhance comments

--- pkg/controller/infrastructure/infraflow/reconcile.go
+++ pkg/controller/infrastructure/infraflow/reconcile.go
@@ -427,12 +427,13 @@ func (c *FlowContext) ensureGatewayEndpoints(ctx context.Context) error {
 		child.Set(c.extractVpcEndpointName(item), created.VpcEndpointId)
 	}
 
-	// Ensure tags on existing endpoints
 	for _, pair := range toBeChecked {
 		child.Set(c.extractVpcEndpointName(pair.current), pair.current.VpcEndpointId)
+		// Ensure tags on existing endpoints
 		if _, err := c.updater.UpdateEC2Tags(ctx, pair.current.VpcEndpointId, pair.desired.Tags, pair.current.Tags); err != nil {
 			return err
 		}
+		// Ensure IpAddressType on existing endpoints
 		if pair.current.IpAddressType != pair.desired.IpAddressType {
 			log.Info("updating ip address type...", "serviceName", pair.current.ServiceName)
 			err = c.client.UpdateVpcEndpointIpAddressType(ctx, pair.current.VpcEndpointId, pair.desired.IpAddressType)
