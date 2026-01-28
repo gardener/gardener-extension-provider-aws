fb3639376833c4083afd4c304e34bed4c7f42894
Alexander Hebel <alexander.hebel@sap.com>
2026-01-28 16:49:45 +0100

    Remove deprecated flow annotation constants (#1670)

--- pkg/apis/aws/const.go
+++ pkg/apis/aws/const.go
@@ -5,20 +5,6 @@
 package aws
 
 const (
-	// AnnotationKeyUseFlow is the annotation key used to enable reconciliation with flow instead of terraformer.
-	AnnotationKeyUseFlow = "aws.provider.extensions.gardener.cloud/use-flow"
-	// GlobalAnnotationKeyUseFlow is the annotation key used to enable reconciliation with flow instead of terraformer.
-	GlobalAnnotationKeyUseFlow = "provider.extensions.gardener.cloud/use-flow"
-	// SeedAnnotationKeyUseFlow is the label for seeds to enable flow reconciliation for all of its shoots if value is `true`
-	// or for new shoots only with value `new`
-	SeedAnnotationKeyUseFlow = AnnotationKeyUseFlow
-	// SeedAnnotationUseFlowValueNew is the value to restrict flow reconciliation to new shoot clusters
-	SeedAnnotationUseFlowValueNew = "new"
 	// AnnotationKeyIPStack is the annotation key to set the IP stack for a DNSRecord.
 	AnnotationKeyIPStack = "dns.gardener.cloud/ip-stack"
-)
-
-var (
-	// ValidFlowAnnotations is a list of all the available annotations to indicate the use of the flow reconciler.
-	ValidFlowAnnotations = []string{AnnotationKeyUseFlow, GlobalAnnotationKeyUseFlow}
 )
