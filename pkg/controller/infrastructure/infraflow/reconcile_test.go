aa5ac937cdc0037e1aa63ed5a856633473aa4d1d
Andreas Erz <andreas.erz@sap.com>
2026-07-11 09:01:13 +0200

    Add unit tests for FlowContext DHCP options

--- pkg/controller/infrastructure/infraflow/reconcile_test.go
+++ pkg/controller/infrastructure/infraflow/reconcile_test.go
@@ -1,8 +1,17 @@
 package infraflow
 
 import (
+	"context"
+
+	core "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	ext "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+
+	awsclient "github.com/gardener/gardener-extension-provider-aws/pkg/aws/client"
+	mockawsclient "github.com/gardener/gardener-extension-provider-aws/pkg/aws/client/mock"
+	"github.com/gardener/gardener-extension-provider-aws/pkg/controller/infrastructure/infraflow/shared"
 )
 
 // Tests for self-contained functions
@@ -63,5 +72,109 @@ var _ = Describe("#calcNextIPv6CidrBlock", func() {
 	It("Should return an error when the input CIDR is invalid", func() {
 		_, err := calcNextIPv6CidrBlock("invalid-cidr")
 		Expect(err).To(HaveOccurred())
+	})
+})
+
+var _ = Describe("#FlowContext", func() {
+	var (
+		tags    awsclient.Tags
+		ctrl    *gomock.Controller
+		client  *mockawsclient.MockInterface
+		updater *mockawsclient.MockUpdater
+		ctx     context.Context
+		c       *FlowContext
+	)
+	BeforeEach(func() {
+		tags = awsclient.Tags{
+			"kubernetes.io/cluster/shoot--myproject--mycluster": "1",
+			"Name": "shoot--myproject--mycluster",
+		}
+		ctrl = gomock.NewController(GinkgoT())
+		client = mockawsclient.NewMockInterface(ctrl)
+
+		// We don't want to test the updater here, so we just mock it to always return false and no error.
+		updater = mockawsclient.NewMockUpdater(ctrl)
+		updater.EXPECT().UpdateVpc(gomock.Any(), gomock.Any(), gomock.Any()).Return(false, nil).AnyTimes()
+		updater.EXPECT().UpdateSecurityGroup(gomock.Any(), gomock.Any(), gomock.Any()).Return(false, nil).AnyTimes()
+		updater.EXPECT().UpdateRouteTable(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(false, nil).AnyTimes()
+		updater.EXPECT().UpdateSubnet(gomock.Any(), gomock.Any(), gomock.Any()).Return(false, nil).AnyTimes()
+		updater.EXPECT().UpdateIAMInstanceProfile(gomock.Any(), gomock.Any(), gomock.Any()).Return(false, nil).AnyTimes()
+		updater.EXPECT().UpdateIAMRole(gomock.Any(), gomock.Any(), gomock.Any()).Return(false, nil).AnyTimes()
+		updater.EXPECT().UpdateEC2Tags(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(false, nil).AnyTimes()
+
+		ctx = context.TODO()
+		c = &FlowContext{
+			state: shared.NewWhiteboard(),
+			infraSpec: ext.InfrastructureSpec{
+				Region: "eu-central-1",
+			},
+			commonTags: tags,
+			client:     client,
+			updater:    updater,
+		}
+	})
+	Describe("#getIPFamilies", func() {
+		It("should return IPv4 when networking is not set", func() {
+			c.networking = nil
+			families := c.getIpFamilies()
+			Expect(families).ToNot(BeNil())
+			Expect(families).To(HaveLen(1))
+			Expect(families).To(ContainElement(core.IPFamilyIPv4))
+		})
+	})
+	Context("DHCP Options", func() {
+		var (
+			dhcpOptionsId     string
+			dhcpOptions       *awsclient.DhcpOptions
+			dhcpOptionsWithId *awsclient.DhcpOptions
+		)
+		BeforeEach(func() {
+			dhcpOptions = &awsclient.DhcpOptions{
+				Tags: tags,
+				DhcpConfigurations: map[string][]string{
+					"domain-name":         {"eu-central-1.compute.internal"},
+					"domain-name-servers": {"AmazonProvidedDNS"},
+				},
+			}
+			dhcpOptionsId = "dopt-084807a4d953f0424"
+			dhcpOptionsWithId = &awsclient.DhcpOptions{}
+			*dhcpOptionsWithId = *dhcpOptions
+			dhcpOptionsWithId.DhcpOptionsId = dhcpOptionsId
+		})
+		Describe("#getDesiredDhcpOptions", func() {
+			It("should set the correct domain name for us-east-1", func() {
+				c.infraSpec.Region = "us-east-1"
+				expectedDhcpOptions := dhcpOptions
+				expectedDhcpOptions.DhcpConfigurations["domain-name"] = []string{"ec2.internal"}
+				desiredDhcpOptions := c.getDesiredDhcpOptions()
+				Expect(desiredDhcpOptions).To(Equal(expectedDhcpOptions))
+			})
+			It("should set the correct domain name for other regions", func() {
+				desiredDhcpOptions := c.getDesiredDhcpOptions()
+				Expect(desiredDhcpOptions).To(Equal(dhcpOptions))
+			})
+		})
+		Describe("#ensureDhcpOptions", func() {
+			It("should create new DHCP options if none exist", func() {
+				client.EXPECT().FindVpcDhcpOptionsByTags(ctx, tags).Return(nil, nil).Times(1)
+				client.EXPECT().CreateVpcDhcpOptions(ctx, dhcpOptions).Return(dhcpOptionsWithId, nil).Times(1)
+				err := c.ensureDhcpOptions(ctx)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(*c.state.Get(IdentifierDHCPOptions)).To(Equal(dhcpOptionsId))
+			})
+			It("should use existing DHCP options if they can be found by tags", func() {
+				client.EXPECT().FindVpcDhcpOptionsByTags(ctx, tags).Return([]*awsclient.DhcpOptions{dhcpOptionsWithId}, nil).Times(1)
+				err := c.ensureDhcpOptions(ctx)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(*c.state.Get(IdentifierDHCPOptions)).To(Equal(dhcpOptionsId))
+			})
+			It("should use existing DHCP options if they can be found by ID", func() {
+				client.EXPECT().GetVpcDhcpOptions(ctx, dhcpOptionsId).Return(dhcpOptionsWithId, nil).Times(1)
+				c.state.Set(IdentifierDHCPOptions, dhcpOptionsId)
+				err := c.ensureDhcpOptions(ctx)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(*c.state.Get(IdentifierDHCPOptions)).To(Equal(dhcpOptionsId))
+			})
+		})
 	})
 })
