ca27720fdf9bcdbc28c56db51179c68b27d4fc4e
hebelsun <alexander.hebel@sap.com>
2026-08-19 15:41:11 +0200

    Make generate

--- imagevector/imagevector.go
+++ imagevector/imagevector.go
@@ -20,10 +20,10 @@ var imageVector imagevector.ImageVector
 func init() {
 	var err error
 
-	imageVector, err = imagevector.Read([]byte(imagesYAML))
+	imageVector, _, err = imagevector.Read([]byte(imagesYAML))
 	runtime.Must(err)
 
-	imageVector, err = imagevector.WithEnvOverride(imageVector, imagevector.OverrideEnv)
+	imageVector, _, err = imagevector.WithEnvOverride(imageVector, nil, imagevector.OverrideEnv)
 	runtime.Must(err)
 }
 
